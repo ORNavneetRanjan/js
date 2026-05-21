77a27b7164afbf4ce69e6b130ca7460e131082cb
ORNavneetRanjan <navneet.ranjan274057@gmail.com>
2026-05-21 22:46:36 +0530

    day 30

--- day30/script.js
+++ day30/script.js
@@ -1,8 +1,17 @@
+import { sum, pi } from "./math.js";
+
 console.log("this is script.js");
-function func(a, b){
-	console.log(a + b);
-};
+
+function func(a, b) {
+  console.log(a + b);
+}
 
 console.log("calling function ");
 func(12, 124);
 
+for (let ele of process.argv) {
+  console.log(ele);
+}
+
+console.log(pi);
+console.log(sum(1, 2));
